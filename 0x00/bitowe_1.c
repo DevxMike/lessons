3f0d08e99af0bf32eaa1c9d3d1b8c1539b66dd31
mike <michalbaz@poczta.onet.pl>
2020-11-09 18:30:18 +0100

    comments

--- 0x00/bitowe_1.c
+++ 0x00/bitowe_1.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdint.h>
-#define A_ZEROS_MASK 0x55555555
-
+#define A_ZEROS_MASK 0x55555555 //0101....
+//~A_ZEROS_MASK = 1010... 
 void test_print(uint32_t pair, uint8_t bits){
     if(bits > 0){
         putchar((pair & (1 << (bits - 1)))? '1' : '0');
@@ -10,18 +10,21 @@ void test_print(uint32_t pair, uint8_t bits){
 }
 
 uint32_t setaz(uint32_t pair){
-    return pair & A_ZEROS_MASK;
+    return pair & A_ZEROS_MASK; 
 }
 uint32_t setbz(uint32_t pair){
     return pair & ~A_ZEROS_MASK;
 }
 uint32_t swap(uint32_t pair){
+    //najpierw przemnazany pare przez maske, aby bity ai byly wyzerowane
+    //przesuwamy w lewo o jedno miejsce nasza liczbe b 111111... 010101... 101010
+    //to samo z a tylko ze w przeciwnym kierunku 111111... 101010... 010101
     return ((pair & A_ZEROS_MASK) << 1) | ((pair & ~A_ZEROS_MASK) >> 1);
 }
 uint32_t seta(uint32_t pair, uint16_t a){
-    pair = setaz(pair);
+    pair = setaz(pair); //wyzerowac skladowe ai 
     for(int i = 31, j = 15; i > 0; i = i - 2, --j){
-        if(a & (1 << j)){
+        if(a & (1 << j)){ // j = 0, a = 1100,  a & (1 << j) = 1100 & 0001 -> 0
             pair |= (1 << i);
         }
     }
@@ -30,13 +33,13 @@ uint32_t seta(uint32_t pair, uint16_t a){
 uint16_t geta(uint32_t pair){
     uint16_t tmp = 0;
     for(int i = 31, j = 15; i > 0; i = i - 2, --j){
-        if(pair & (1 << i)){
+        if(pair & (1 << i)){ // i = 31 (a15), pair = 1010.... & (1 << i) = 0010.... & 1000.... -> 0
             tmp |= (1 << j);
         }
     }
     return tmp;
 }
-uint32_t join(uint16_t a, uint16_t b){
+uint32_t join(uint16_t a, uint16_t b){ //1010, 1010 >> 1, bi 0101
     return seta(((seta(0, b)) >> 1), a);
 }
 uint32_t add(uint32_t pair){
@@ -49,6 +52,9 @@ int main(void){
     uint32_t test = 0xFFFFFFFF;
     uint16_t test_a = 0x1;
     uint16_t test_b = 0xA;
+    printf("%20s", "Przed zerowaniem a: ");
+    test_print(test, 32);
+    putchar('\n');
     test = setaz(test);
     printf("%20s", "Przed zamiana: ");
     test_print(test, 32);
