63af22187b6310a6a54c3ddacf8bf313a57f6f08
mike <michalbaz@poczta.onet.pl>
2021-10-13 17:07:57 +0200

    homework

--- 0x05/13_10.cpp
+++ 0x05/13_10.cpp
@@ -3,6 +3,17 @@
 using namespace std;
 
 int main(void){
+    int tablica[10]{ 10, 21, 34, 4, 51, 64, 71, 85, 91, 110 }; //jeśli chcesz odnaleźć jakiś element w tablicy
+    // można zastosować następujące postępowanie:
+    // sortowanie tablicy a następnie przeszukiwanie jej
 
+    int porzadany_el;
+
+    cin >> porzadany_el;
     
+    for(int i = 0; i < 10; ++i){
+        if(tablica[i] == porzadany_el){
+            cout << "Element " << porzadany_el << " znajduje sie pod indeksem " << i << endl;
+        }
+    }
 }
