0b0d7334cda9f8e69deede26c104389faef87fe9
mike <michalbaz@poczta.onet.pl>
2021-10-19 17:33:56 +0200

    ex 3 & 6

---
+++ 0x06/zad1.c
@@ -0,0 +1,117 @@
+#include <math.h>
+#include <stdio.h> //std - standard, i-input, o-output
+
+int main(){
+    return 0;
+}
+
+/*zadanie 3 new 
+{
+        double a, b, c, d;
+
+        printf("Podawaj liczby w notacji wykladniczej. \n");
+
+        printf("Podaj pierwsza liczbe: ");
+        scanf("%lf", &a);
+        printf("Twoja liczba to: %.2lf \n", a);
+
+        printf("Podaj druga liczbe: ");
+        scanf("%lf", &b);
+        printf("Twoja druga liczba to: %.2lf \n", b);
+
+        printf("Podaj trzecia liczbe: ");
+        scanf("%lf", &c);
+        printf("Twoja trzecia liczba to: %.2lf \n", c);
+
+        printf("Podaj czwarta liczbe: ");
+        scanf("%lf", &d);
+        printf("Twoja czwarta liczba to: %.2lf \n", d);
+
+        double dodawanie = a + b;
+        double odejmowanie = c - d;
+
+        printf("Wynik odejmowania liczb %.2lf i %.2lf to %.2lf \n", c, d, odejmowanie);
+        printf("Wynik dodawania liczb %.2lf i %.2lf to %.2lf \n", a, b, dodawanie);
+
+    return 0;
+}
+*/
+
+/*{ //zadanie 6 new 
+    double a;
+    double pierwiastek;
+    double potega;
+    double wartoscBezwzgledna;
+    double zaokraglenia1, zaokraglenia2;  
+
+    printf("Podaj pierwsza liczbe: ");
+    scanf("%lf", &a);
+    pierwiastek = sqrt(a);
+    printf("Podaj druga liczbe: ");
+    scanf("%lf", &a);
+    potega = pow(a, 2);
+    printf("Podaj trzecia liczbe: ");
+    scanf("%lf", &a);
+    wartoscBezwzgledna = fabs(a);
+    printf("Podaj czwarta liczbe: ");
+    scanf("%lf", &a);
+    zaokraglenia1 = floor(a);
+    zaokraglenia2 = ceil(a);
+
+    printf("sqrt = %lf, pow = %lf, fabs = %lf\n", pierwiastek, potega, wartoscBezwzgledna);
+    printf("orig = %lf, floor = %lf, ceil = %lf\n", a, zaokraglenia1, zaokraglenia2);
+
+    return 0;
+}*/
+
+/*// zad. 6.
+    {
+        double a, b, c, d;
+        double pierwiastek;
+        double potega;
+        double wartoscBezwzgledna;
+        double zaokraglenia;
+
+        printf("Podaj pierwsza liczbe: ");
+        scanf("%f", a);
+        printf("Podaj druga liczbe: ");
+        scanf("%f", b);
+        printf("Podaj trzecia liczbe: ");
+        scanf("%f", c);
+        printf("Podaj czwarta liczbe: ");
+        scanf("%f", d);
+
+        sqrt(a);
+    }
+    //*/
+
+/*// zad. 3.
+    {
+        double a, b, c, d;
+        double dodawanie = a + b;
+        double odejmowanie = c - d;
+
+        printf("Podawaj liczby w notacji wykladniczej. \n");
+
+        printf("Podaj pierwsza liczbe: ");
+        scanf("%lf", &a);
+        printf("Twoja liczba to: %.2lf \n", a);
+
+        printf("Podaj druga liczbe: ");
+        scanf("%lf", &b);
+        printf("Twoja druga liczba to: %.2lf \n", b);
+
+        printf("Podaj trzecia liczbe: ");
+        scanf("%lf", &c);
+        printf("Twoja trzecia liczba to: %.2lf \n", c);
+
+        printf("Podaj czwarta liczbe: ");
+        scanf("%lf", &d);
+        printf("Twoja czwarta liczba to: %.2lf \n", d);
+
+        printf("Wynik odejmowania liczb %.2lf i %.2lf to %.2lf \n", a, b, odejmowanie);
+        printf("Wynik dodawania liczb %.2lf i %.2lf to %.2lf \n", c, d, dodawanie);
+
+        return 0; //co jest zle?
+    }
+    //*/
