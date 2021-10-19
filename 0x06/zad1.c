615a026a67d5a130fa3665f6b1c5d36ddea9bb08
mike <michalbaz@poczta.onet.pl>
2021-10-19 18:16:49 +0200

    end

--- 0x06/zad1.c
+++ 0x06/zad1.c
@@ -1,9 +1,104 @@
 #include <math.h>
 #include <stdio.h> //std - standard, i-input, o-output
+#include <stdbool.h> //typ bool logiczny
 
 int main(){
+    int R, G, B;
+
+    printf("Podaj R: ");
+    scanf("%d", &R);
+
+
+    printf("Podaj G: ");
+    scanf("%d", &G);
+
+
+    printf("Podaj B: ");
+    scanf("%d", &B);
+
+    if(R == 0 && G == 0 && B == 0){
+        printf("czarny\n");
+    }
+    else if(R == 0 && G == 0 && B == 1){
+        printf("niebieski\n");
+    }
+    else if(R == 0 && G == 1 && B == 0){
+        printf("zielony\n");
+    }
+    else if(R && !G && !B){ //to samo co R == 1 && G == 0 && B == 0
+        printf("czerwony\n");
+    }
+    else if(R && !G && B){
+        printf("fioletowy\n");
+    }
+    else if(R && G && !B){
+        printf("zolty\n");
+    }
+    else if(R && G && B){
+        printf("bialy\n"); 
+    }
     return 0;
 }
+
+/* zadanie 4 drugi arkusz
+    if(wyrazenie){ //jesli wyrazenie jest prawdziwe 
+        //instrukcje, ktore maja sie wykonac gdy wyrazenie jest prawdziwe
+    }
+    else{ //w przeciwnym wypadku gdy wyrazenie nie jest prawdziwe
+        //instrukcje, ktore maja sie wykonac gdy wyrazenie nie jest prawdziwe
+    }
+    
+
+    if(wyrazenie1){
+        //instrukcje
+    }    
+    else if(wyrazenie2){
+        //instrukcje
+    }
+    else if(wyrazenien){
+        //instrukcje
+    }
+    else{
+
+    }
+
+    if(wyrazenie){
+        //instrukcje
+    }
+
+    if(wyrazenie1){
+        //instrukcje
+    }    
+    else if(wyrazenie2){
+        //instrukcje
+    }
+    else if(wyrazenien){
+        //instrukcje
+    } 
+    */
+    /*double liczba;
+
+    printf("Podaj liczbe: ");
+    scanf("%lf", &liczba);
+
+        if(liczba < -10){
+        printf("czerwony\n");
+        }
+        else if(liczba > -10 && liczba < 0){
+            printf("zielony\n");
+        }
+        else if(liczba > 0 && liczba < 10){
+            printf("niebieski\n");
+        }
+        else if(liczba > 10){
+            printf("zolty\n");
+        }
+        else{
+            printf("czarny\n");
+        }
+
+    return 0;
+*/
 
 /*zadanie 3 new 
 {
